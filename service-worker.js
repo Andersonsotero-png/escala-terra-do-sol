a74c792c085dbf9fb033f83e6407cff6e1dab164
Parquinho tds. <anderson.sotero@aluno.unifametro.edu.br>
2025-11-26 19:20:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,30 @@
-self.addEventListener('install', e=>{self.skipWaiting();});
+const CACHE_NAME = 'parquinho-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/manifest.json',
+  '/assets/img/logo-terra-do-sol.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => {
+      return resp || fetch(evt.request);
+    })
+  );
+});
